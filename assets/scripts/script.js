545ebe3fa0a499e884dd2b1b59e99611eabf3147
samrahajiyeva <universes.girl15@gmail.com>
2025-03-06 23:57:29 +0400

    update

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,16 +1,32 @@
 //  BISMILLAH
 
+document.addEventListener("DOMContentLoaded", function () {
+  // Modal ucun kodlar
+  const searchBtn = document.querySelector(".search-btn");
+  const modal = document.querySelector(".modal");
+  const modalCloser = document.querySelector(".modal-closer");
 
-// Modal ucun kodlar
-const searchBtn = document.querySelector('.search-btn')
-const modal = document.querySelector('.modal')
-const modalCloser = document.querySelector('.modal-closer')
+  searchBtn.addEventListener("click", (e) => {
+    modal.classList.add("m-display");
+  });
 
+  modalCloser.addEventListener("click", (e) => {
+    modal.classList.remove("m-display");
+  });
 
-searchBtn.addEventListener('click' , (e)=> {
-    modal.classList.add('m-display')    
-})
+  //hero footer ucun kodlar
+  const icon = document.querySelector(".hero-footer-toggle");
+  const footerHero = document.querySelector(
+    ".postman__main__hero__bottom__left__footer__hero"
+  );
 
-modalCloser.addEventListener('click' , (e)=> {
-    modal.classList.remove('m-display')
-})
+  icon.addEventListener("click", function () {
+    if (footerHero.style.display === "none") {
+      footerHero.style.display = "block";
+      icon.classList.replace("fa-angle-up", "fa-angle-down");
+    } else {
+      footerHero.style.display = "none";
+      icon.classList.replace("fa-angle-down", "fa-angle-up");
+    }
+  });
+});
