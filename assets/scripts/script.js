eccd2f77f6549fd3f7a9074bedf19f18ef2f9d58
AVH <avh@avh.b>
2025-03-06 17:04:51 +0400

    adding modal

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,1 +1,16 @@
 //  BISMILLAH
+
+
+// Modal ucun kodlar
+const searchBtn = document.querySelector('.search-btn')
+const modal = document.querySelector('.modal')
+const modalCloser = document.querySelector('.modal-closer')
+
+
+searchBtn.addEventListener('click' , (e)=> {
+    modal.classList.add('m-display')    
+})
+
+modalCloser.addEventListener('click' , (e)=> {
+    modal.classList.remove('m-display')
+})
